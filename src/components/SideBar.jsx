ca88a5a0592bd63a352ecfa7f5d767c738388217
nikita3006 <nikitanegi3011@gmail.com>
2024-04-22 11:46:42 +0530

    created sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,90 @@
-import { Box } from "@mui/material"
-
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from "@mui/material";
+import {useState} from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import ArticleIcon from '@mui/icons-material/Article';
+import GroupsIcon from '@mui/icons-material/Groups';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PersonIcon from '@mui/icons-material/Person';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import Brightness5Icon from '@mui/icons-material/Brightness5';
 
 const SideBar = () => {
+  const[click, setClick] =useState(false)
+
+  const handleClick = () =>{
+    setClick(!click)
+  }
   return (
-    <Box bgcolor='red' flex={1} p={2} sx={{ display:{ xs : 'none', sm:'block'}}}>SideBar</Box>
+    <Box  flex={1} p={2} sx={{ display:{ xs : 'none', sm:'block'}}}>
+       <List>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#' >
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+          <ListItemButton component='a' href='#' >
+              <ListItemIcon>
+                <ArticleIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#' >
+              <ListItemIcon>
+                <GroupsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#' >
+              <ListItemIcon>
+                <StorefrontIcon />
+              </ListItemIcon>
+              <ListItemText primary="MarketPlace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#' >
+              <ListItemIcon>
+                <PersonIcon />
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#' >
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Setting" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#' >
+              <ListItemIcon>
+                <AccountCircleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Account" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#' >
+              <ListItemIcon onClick={handleClick}>
+               {click ?  <DarkModeIcon /> :  <Brightness5Icon/>}
+              </ListItemIcon>
+              <Switch  defaultChecked />
+            </ListItemButton>
+          </ListItem>
+        </List>
+    </Box>
   )
 }
 
